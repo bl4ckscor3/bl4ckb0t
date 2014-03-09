12f92ed0a6fa87c2a588260436506c2c28cb2e73
bl4ckscor3 <donalddani9612@gmx.de>
2014-03-09 03:04:39 +0100

    fixed issue with -kick

--- bl4ckb0tX/src/bl4ckb0tX/commands/normal/Kick.java
+++ bl4ckb0tX/src/bl4ckb0tX/commands/normal/Kick.java
@@ -10,59 +10,67 @@ public class Kick
 {	
 	public static void exe(MessageEvent event)
 	{
-		boolean allowed = false;
-		boolean found = false;
-		String[] allowedUsers = 
-			{
-				"bl4ckscor3",
-				"bl4ckgon3",
-				"bl4ckweb",
-				"bl4ckdro1d",
-				"Darkhax",
-				"Geforce132",
-				"Geforce132|Away",
-				"TehKitti"
-			};
-		//-kick bl4ckscor3 cause i can
-		String splitUserReasonFromCommand = Stuffz.getMessage(event).substring(5);// bl4ckscor3 cause i can
-		String correctSplitFromCommand = splitUserReasonFromCommand.substring(1);//bl4ckscor3 cause i can
-		String[] splitUserFromReason = correctSplitFromCommand.split(" ");//bl4ckscor3
-		String splitReasonFromUser = correctSplitFromCommand.substring(splitUserFromReason[0].length());//cause i can
-		String user = splitUserFromReason[0];
-		String reason = splitReasonFromUser;
-
-		for(int i = 0; i < allowedUsers.length; i++)
+		if(event.getUser().isVerified())
 		{
-			if(Stuffz.getNick(event).equals(allowedUsers[i]))
-			{	
-				allowed = true;
-				break;
-			}
-		}
-
-		if(!allowed)
-		{
-			Stuffz.chanMsg(event, "Sorry, " + Stuffz.getNick(event) + ", you're not authorized to kick people from this channel.");
-		}
-		else
-		{
-			for(int k = 0; k < allowedUsers.length; k++)
-			{
-				if(user.equalsIgnoreCase(allowedUsers[k]))
+			boolean allowed = false;
+			boolean found = false;
+			String[] allowedUsers = 
 				{
-					found = true;
+					"bl4ckscor3",
+					"bl4ckgon3",
+					"bl4ckweb",
+					"bl4ckdro1d",
+					"Darkhax",
+					"Geforce132",
+					"Geforce132|Away",
+					"TehKitti",
+					"testingstuffz"
+				};
+			//-kick bl4ckscor3 cause i can
+			String splitUserReasonFromCommand = Stuffz.getMessage(event).substring(5);// bl4ckscor3 cause i can
+			String correctSplitFromCommand = splitUserReasonFromCommand.substring(1);//bl4ckscor3 cause i can
+			String[] splitUserFromReason = correctSplitFromCommand.split(" ");//bl4ckscor3
+			String splitReasonFromUser = correctSplitFromCommand.substring(splitUserFromReason[0].length());//cause i can
+			String user = splitUserFromReason[0];
+			String reason = splitReasonFromUser;
+
+			for(int i = 0; i < allowedUsers.length; i++)
+			{
+				if(Stuffz.getNick(event).equals(allowedUsers[i]))
+				{	
+					allowed = true;
+					break;
 				}
 			}
 
-			if(!found)
+			if(!allowed)
 			{
-				event.getChannel().send().action("kicks " + user);
-				Core.bot.sendRaw().rawLine("KICK " + event.getChannel().getName() + " " + user + " :" + reason.substring(1));
+				Stuffz.chanMsg(event, "Sorry, " + Stuffz.getNick(event) + ", you're not authorized to kick people from this channel.");
 			}
 			else
 			{
-				Stuffz.chanMsg(event, "This user cannot be kicked.");
+				for(int k = 0; k < allowedUsers.length; k++)
+				{
+					if(user.equalsIgnoreCase(allowedUsers[k]))
+					{
+						found = true;
+					}
+				}
+
+				if(!found)
+				{
+					event.getChannel().send().action("kicks " + user);
+					Core.bot.sendRaw().rawLine("KICK " + event.getChannel().getName() + " " + user + " :" + reason.substring(1));
+				}
+				else
+				{
+					Stuffz.chanMsg(event, "This user cannot be kicked.");
+				}
 			}
+		}
+		else
+		{
+			Stuffz.respond(event, "you need to be identified to be able to kick someone.");
 		}
 	}
 }
