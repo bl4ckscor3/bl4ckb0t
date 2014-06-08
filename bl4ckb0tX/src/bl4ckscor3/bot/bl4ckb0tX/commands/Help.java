9a9ebd0443cfab01d19a9cfd9ea8c3c75c5097cf
bl4ckscor3 <donalddani9612@gmx.de>
2014-06-08 15:05:48 +0200

    updated help menu

--- bl4ckb0tX/src/bl4ckscor3/bot/bl4ckb0tX/commands/Help.java
+++ bl4ckb0tX/src/bl4ckscor3/bot/bl4ckb0tX/commands/Help.java
@@ -7,21 +7,34 @@ import bl4ckscor3.bot.bl4ckb0tX.util.Utilities;
 
 public class Help implements Command<MessageEvent>
 {
+	private String color = Colors.BOLD + Colors.BLUE;
+	
 	@Override
 	public void exe(MessageEvent event) //TODO: Update this menu!!
 	{
-		Utilities.userMsg(event, Colors.BOLD + Colors.OLIVE + "----------------------My Commands :) (Outdated)----------------------");
-		Utilities.addHelpLine(event, "-letter <number> " + Colors.BOLD + Colors.BLUE + "| Outputs as many random letters as you specified (not more than 20)!");
-		Utilities.addHelpLine(event, "-number <number> " + Colors.BOLD + Colors.BLUE + "| Outputs as many random numbers as you specified (not more than 20)!");
-		Utilities.addHelpLine(event, "-u <name> [where to go] " + Colors.BOLD + Colors.BLUE + "| Minecraftforumuser command!");
-		Utilities.addHelpLine(event, "-yt <channel> " + Colors.BOLD + Colors.BLUE + "| Sends you to the specified YouTube channel!");
-		Utilities.addHelpLine(event, "-tw <profile> " + Colors.BOLD + Colors.BLUE + "| Sends you to the specified Twitter profile!");
-		Utilities.addHelpLine(event, "-tv <channel> " + Colors.BOLD + Colors.BLUE + "| Sends you to the specified Twitch channel!");
-		Utilities.addHelpLine(event, "-kick <user> <reason> " + Colors.BOLD + Colors.BLUE + "| Kicks a user from a channel. Only useable by specific people");
-		Utilities.addHelpLine(event, "-draw " + Colors.BOLD + Colors.BLUE + "| Draws something on the screen!");
-		Utilities.addHelpLine(event, "-latestforge " + Colors.BOLD + Colors.BLUE + "| Several commands to receive the changelog, source and installer.");
-		Utilities.addHelpLine(event, "-decide <question> " + Colors.BOLD + Colors.BLUE + "| Decides for you. Please only enter Yes/No answerable questions.");
-		Utilities.addHelpLine(event, "-select <options> " + Colors.BOLD + Colors.BLUE + "| Decides for you. Please only enter options seperated with.");
+		System.out.println(1);
+		Utilities.userMsg(event, Colors.BOLD + Colors.OLIVE + "----------------------My Commands :)----------------------");
+		Utilities.addHelpLine(event, "-bukkit <command> " + color + "| Several commands to receive the recommended, beta, and dev bukkit builds.");
+		Utilities.addHelpLine(event, "-cbukkit <command> " + color + "| Several commands to receive the recommended, beta, and dev craftbukkit builds.");
+		Utilities.addHelpLine(event, "-changenick <newNick>"  + color + "| Changes the bot's nickname. Only userable by specific people.");
+		Utilities.addHelpLine(event, "-decide <question> " + color + "| Decides for you. Please only enter yes/no answerable questions.");
+		Utilities.addHelpLine(event, "-disable " + color + "| Disables all the bot's features. Only useable by specific people.");
+		Utilities.addHelpLine(event, "-draw <drawing> " + color + "| Draws something on the screen!");
+		Utilities.addHelpLine(event, "-enable " + color + "| Enables all the bot's features. Only useable by specific people.");
+		Utilities.addHelpLine(event, "-girlballs " + color + "| Girlballs.");
+		Utilities.addHelpLine(event, "-kick <user> <reason> " + color + "| Kicks a user from a channel. Only useable by specific people.");
+		Utilities.addHelpLine(event, "-latestforge <command> " + color + "| Several commands to receive the changelog, source and installer.");
+		Utilities.addHelpLine(event, "-leet <sentence> " + color + "| Converts your sentence into leet.");
+		Utilities.addHelpLine(event, "-letter <number> " + color + "| Outputs as many random letters as you specified (not more than 20)!");
+		Utilities.addHelpLine(event, "-number <number> " + color + "| Outputs as many random numbers as you specified (not more than 20)!");
+		Utilities.addHelpLine(event, "-select <options> " + color + "| Selects an option you specified for you. Please only enter options seperated with a comma.");
+		Utilities.addHelpLine(event, "-source" + color + "| Gives you the link to the bot's sourcecode.");
+		Utilities.addHelpLine(event, "-stop <yes|no> " + color + "| Stops the bot with or without a restart. Only useable by specific people.");
+		Utilities.addHelpLine(event, "-tv <channel> " + color + "| Sends you to the specified Twitch channel!");
+		Utilities.addHelpLine(event, "-tw <profile> " + color + "| Sends you to the specified Twitter profile!");
+		Utilities.addHelpLine(event, "-u <name> [where to go] " + color + "| Minecraftforumuser command!");
+		Utilities.addHelpLine(event, "-w <city> " + color + "| Shows you the current weather stats of the specified city.");
+		Utilities.addHelpLine(event, "-yt <channel> " + color + "| Sends you to the specified YouTube channel!");
 		Utilities.addEmptyLine(event, false);
 		Utilities.userMsg(event, Colors.BOLD + Colors.OLIVE + "-------------------------Credits--------------------------");
 		Utilities.userMsg(event, Colors.BLUE + "Made by bl4ckscor3!");
--- bl4ckb0tX/src/bl4ckscor3/bot/bl4ckb0tX/core/Core.java
+++ bl4ckb0tX/src/bl4ckscor3/bot/bl4ckb0tX/core/Core.java
@@ -9,22 +9,23 @@ import org.pircbotx.exception.IrcException;
 public class Core
 {	
 	public static PircBotX bot;
-	private static Configuration config = new Configuration.Builder()	
-	.setName("bl4ckb0t")
-	.setVersion("1.0")
-	.setServerHostname("availo.esper.net")
-	.setServerPort(6667)
-	.setLogin("bl4ckb0t")
-	.addAutoJoinChannel("#bl4ckscor3")
-	.setNickservPassword("xxx")
-	.setAutoNickChange(true)
-	.setCapEnabled(true)
-	.addListener(new Listener())
-	.setMessageDelay(500)
-	.buildConfiguration();
 
 	public static void main(String args[]) throws IOException, IrcException
 	{
+		Configuration config = new Configuration.Builder()	
+		.setName("bl4ckb0t")
+		.setVersion("1.0")
+		.setServerHostname("availo.esper.net")
+		.setServerPort(6667)
+		.setLogin("bl4ckb0t")
+		.addAutoJoinChannel("#bl4ckscor3")
+		.setNickservPassword("xxx")
+		.setAutoNickChange(true)
+		.setCapEnabled(true)
+		.addListener(new Listener())
+		.setMessageDelay(500)
+		.buildConfiguration();
+		
 		bot = new PircBotX(config);
 
 		if(!bot.isConnected())
@@ -43,6 +44,20 @@ public class Core
 
 	public static void main2()
 	{
+		Configuration config = new Configuration.Builder()	
+		.setName("bl4ckb0t")
+		.setVersion("1.0")
+		.setServerHostname("availo.esper.net")
+		.setServerPort(6667)
+		.setLogin("bl4ckb0t")
+		.addAutoJoinChannel("#bl4ckscor3")
+		.setNickservPassword("xxx")
+		.setAutoNickChange(true)
+		.setCapEnabled(true)
+		.addListener(new Listener())
+		.setMessageDelay(500)
+		.buildConfiguration();
+		
 		bot = new PircBotX(config);
 
 		if(!bot.isConnected())
