1603b4bee04d481a3ee1641c980b3b20682589e1
bl4ckscor3 <donalddani9612@gmx.de>
2014-07-18 18:46:44 +0200

    added autoJoinChan list

--- bl4ckb0tX/src/bl4ckscor3/bot/bl4ckb0tX/core/Core.java
+++ bl4ckb0tX/src/bl4ckscor3/bot/bl4ckb0tX/core/Core.java
@@ -1,8 +1,13 @@
 package bl4ckscor3.bot.bl4ckb0tX.core;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.pircbotx.Configuration;
+import org.pircbotx.Configuration.Builder;
 import org.pircbotx.PircBotX;
 import org.pircbotx.exception.IrcException;
 
@@ -12,20 +17,26 @@ public class Core
 
 	public static void main(String args[]) throws IOException, IrcException
 	{
-		Configuration config = new Configuration.Builder()	
+		Configuration config;
+		String[] chans = addAutoJoinChans();
+		Builder configPresets = new Configuration.Builder()	
 		.setName("bl4ckb0t")
 		.setVersion("1.0")
-		.setServerHostname("availo.esper.net")
+		.setServerHostname("irc.esper.net")
 		.setServerPort(6667)
 		.setLogin("bl4ckb0t")
-		.addAutoJoinChannel("#bl4ckscor3")
 		.setNickservPassword("xxx")
 		.setAutoNickChange(true)
 		.setCapEnabled(true)
 		.addListener(new Listener())
-		.setMessageDelay(500)
-		.buildConfiguration();
+		.setMessageDelay(500);
 		
+		for(String s : chans)
+		{
+			configPresets.addAutoJoinChannel(s);
+		}
+		
+		config = configPresets.buildConfiguration();
 		bot = new PircBotX(config);
 
 		if(!bot.isConnected())
@@ -47,20 +58,26 @@ public class Core
 
 	public static void main2()
 	{
-		Configuration config = new Configuration.Builder()	
+		Configuration config;
+		String[] chans = addAutoJoinChans();
+		Builder configPresets = new Configuration.Builder()	
 		.setName("bl4ckb0t")
 		.setVersion("1.0")
-		.setServerHostname("availo.esper.net")
+		.setServerHostname("irc.esper.net")
 		.setServerPort(6667)
 		.setLogin("bl4ckb0t")
-		.addAutoJoinChannel("#bl4ckscor3")
 		.setNickservPassword("xxx")
 		.setAutoNickChange(true)
 		.setCapEnabled(true)
 		.addListener(new Listener())
-		.setMessageDelay(500)
-		.buildConfiguration();
+		.setMessageDelay(500);
 		
+		for(String s : chans)
+		{
+			configPresets.addAutoJoinChannel(s);
+		}
+		
+		config = configPresets.buildConfiguration();
 		bot = new PircBotX(config);
 
 		if(!bot.isConnected())
@@ -72,8 +89,17 @@ public class Core
 					if(!Listener.stopped)
 						bot.startBot();
 				}
-				catch(Exception e){}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
 			}
 		}
+	}
+	
+	private static String[] addAutoJoinChans() throws MalformedURLException, IOException
+	{
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new URL("https://www.dropbox.com/s/owng5ehcvf0h5cr/bl4ckb0t%20chans.txt?dl=1").openStream()));
+		return reader.readLine().split(",");
 	}
 }
