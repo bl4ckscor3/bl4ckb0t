a216a5e0f2089a624bd4547b383937bffc4674e6
bl4ckscor3 <donalddani9612@gmx.de>
2014-09-04 18:44:12 +0200

    no more lowercased output

--- bl4ckb0tX/src/bl4ckscor3/bot/bl4ckb0tX/commands/Leet.java
+++ bl4ckb0tX/src/bl4ckscor3/bot/bl4ckb0tX/commands/Leet.java
@@ -9,22 +9,22 @@ public class Leet implements ICommand<MessageEvent>
 	@Override
 	public void exe(MessageEvent event)
 	{
-		char[] chars = event.getMessage().substring(6).toLowerCase().toCharArray();
+		char[] chars = event.getMessage().substring(6).toCharArray();
 		StringBuilder builder = new StringBuilder();
 		
 		for(int i = 0; i < chars.length; i++)
 		{
 			switch(chars[i])
 			{
-				case 'a': builder.append("4"); break;
-				case 'e': builder.append("3"); break;
-				case 'g': builder.append("6"); break;
-				case 'h': builder.append("h"); break;
-				case 'i': builder.append("1"); break;
-				case 'l': builder.append("|"); break;
-				case 's': builder.append("5"); break;
-				case 't': builder.append("7"); break;
-				case 'z': builder.append("2"); break;
+				case 'A': case 'a': builder.append("4"); break;
+				case 'E': case 'e': builder.append("3"); break;
+				case 'G': case 'g': builder.append("6"); break;
+				case 'H': case 'h': builder.append("h"); break;
+				case 'I': case 'i': builder.append("1"); break;
+				case 'L': case 'l': builder.append("|"); break;
+				case 'S': case 's': builder.append("5"); break;
+				case 'T': case 't': builder.append("7"); break;
+				case 'Z': case 'z': builder.append("2"); break;
 				default: builder.append(chars[i]);
 			}
 		}
