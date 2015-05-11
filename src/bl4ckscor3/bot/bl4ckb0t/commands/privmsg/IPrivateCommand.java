d4260058cc4c864f75f83f8b66fd290f125df385
bl4ckscor3 <donalddani9612@gmx.de>
2015-05-11 18:38:58 +0200

    sending all pms to all valid users

--- src/bl4ckscor3/bot/bl4ckb0t/commands/privmsg/IPrivateCommand.java
+++ src/bl4ckscor3/bot/bl4ckb0t/commands/privmsg/IPrivateCommand.java
@@ -1,12 +1,15 @@
 package bl4ckscor3.bot.bl4ckb0t.commands.privmsg;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 import org.pircbotx.hooks.Event;
 
 import bl4ckscor3.bot.bl4ckb0t.core.Bot;
 
 public interface IPrivateCommand<E extends Event<Bot>>
 {
-	public void exe(E event);
+	public void exe(E event) throws MalformedURLException, IOException;
 	
 	public String getAlias();
 }
--- src/bl4ckscor3/bot/bl4ckb0t/commands/privmsg/UserMsg.java
+++ src/bl4ckscor3/bot/bl4ckb0t/commands/privmsg/UserMsg.java
@@ -1,15 +1,21 @@
 package bl4ckscor3.bot.bl4ckb0t.commands.privmsg;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.pircbotx.hooks.events.PrivateMessageEvent;
 
 import bl4ckscor3.bot.bl4ckb0t.core.Bot;
 import bl4ckscor3.bot.bl4ckb0t.core.Core;
+import bl4ckscor3.bot.bl4ckb0t.util.Lists;
 import bl4ckscor3.bot.bl4ckb0t.util.Utilities;
 
 public class UserMsg implements IPrivateCommand<PrivateMessageEvent<Bot>>
 {
 	@Override
-	public void exe(PrivateMessageEvent<Bot> event)
+	public void exe(PrivateMessageEvent<Bot> event) throws MalformedURLException, IOException
 	{
 		String[] args = Utilities.toArgs(event.getMessage());
 		String msg = "";
@@ -20,6 +26,19 @@ public class UserMsg implements IPrivateCommand<PrivateMessageEvent<Bot>>
 		}
 
 		msg = msg.substring(0, msg.length() - 1);
+		
+		if(Utilities.isValidUser(event))
+		{
+			List<String> users = (ArrayList<String>) Lists.getValidUsers();
+			
+			users.remove(event.getUser().getNick());
+			
+			for(String user : users)
+			{
+				Core.bot.sendCustomMessage(user, event.getUser().getNick() + " to " + args[1] + ": " + msg);
+			}
+		}
+		
 		Core.bot.sendCustomMessage(args[1], msg);
 	}
 
