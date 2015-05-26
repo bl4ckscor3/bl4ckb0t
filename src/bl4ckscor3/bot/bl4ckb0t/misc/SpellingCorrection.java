efa78ec448a8409d7b76da2085a3f81b5732afbb
bl4ckscor3 <donalddani9612@gmx.de>
2015-05-26 18:27:50 +0200

    reverted spellingcorrection back to previous italics replacement

--- src/bl4ckscor3/bot/bl4ckb0t/misc/SpellingCorrection.java
+++ src/bl4ckscor3/bot/bl4ckb0t/misc/SpellingCorrection.java
@@ -18,87 +18,8 @@ public class SpellingCorrection
 	//needed to check if the message should be added to the array or not (used in Listener.java)
 	public static boolean corrected = false;
 
-	private static void correctSpelling(MessageEvent<Bot> event, String[] split, boolean correctsDifferentUser, String userToCorrect)
-	{
-		String toReplace = split[1];
-		String replaceWith = split[2];
-
-		for(String s : storage.get(event.getChannel().getName()))
-		{
-			if(s == null)
-				break;
-
-			if(userToCorrect.equals(s.split("#")[0]))
-			{
-				String previousMessage = getLatestMessage(userToCorrect, storage.get(event.getChannel().getName()));
-				String newMessage = getLatestMessage(userToCorrect, storage.get(event.getChannel().getName())).replace(toReplace, replaceWith); //w/o italics
-				String correctedMessage = addItalics(previousMessage, newMessage);
-
-				if(previousMessage.equals(newMessage))
-					return;
-
-				if(correctsDifferentUser)
-					Utilities.chanMsg(event, Colors.NORMAL + userToCorrect + " " + L10N.getString("correction.1") + " " + event.getUser().getNick() + " " + L10N.getString("correction.2") + ": " + correctedMessage);
-				else
-					Utilities.chanMsg(event, Colors.NORMAL + userToCorrect + " " + L10N.getString("correction.3") + ": " + correctedMessage);
-
-				updateLatestMessage(event.getChannel().getName(), newMessage, userToCorrect);
-			}
-		}
-	}
-
-	/**
-	 * Adds or updates a user with his latest message
-	 * 
-	 * @param msg - The message to write into the array
-	 * @param username - The name of the user who wrote the message
-	 */
-	public static void updateLatestMessage(String channel, String msg, String username)
-	{
-		int i = 0;
-
-		if(!storage.containsKey(channel))
-			storage.put(channel, new ArrayList<String>());
-
-		for(String s : storage.get(channel))
-		{ 
-			//if the current array position contains no data to replace, stop the loop and add the data
-			if(s == null)
-				break;
-
-			//checking for the correct array position to potentially replace the latest message with
-			if(s.split("#")[0].equals(username))
-			{
-				storage.get(channel).set(i, username + "#" + msg);
-				return;
-			}
-
-			i++;
-		}
-
-		storage.get(channel).add(username + "#" + msg);
-	}
-
-	/**
-	 * Returns the latest message from the given user
-	 * 
-	 * @param user - The name of the user to get the latest message from
-	 * @return - The latest message from the given user
-	 */
-	private static String getLatestMessage(String user, List<String> messages)
-	{
-		for(String s : messages)
-		{
-			if(s.split("#")[0].equals(user))
-				return s.split("#")[1];
-		}
-
-		return "Will never happen.";
-	}
-
 	/**
 	 * Determine if the message contains a syntax to correct a message sent before
-	 * 
 	 * @param event - The triggered MessageEvent
 	 * @param message - The message sent to the channel which triggered the event
 	 */
@@ -134,7 +55,7 @@ public class SpellingCorrection
 
 				if(newMessage.endsWith("/"))
 					newMessage += " ";
-
+				
 				split = newMessage.split("/");
 
 				if(split.length == 3 && split[0].equals("s"))
@@ -166,111 +87,80 @@ public class SpellingCorrection
 				return;
 		}
 	}
+	
+	private static void correctSpelling(MessageEvent<Bot> event, String[] split, boolean correctsDifferentUser, String userToCorrect)
+	{
+		String toReplace = split[1];
+		String replaceWith = split[2];
+
+		for(String s : storage.get(event.getChannel().getName()))
+		{
+			if(s == null)
+				break;
+
+			if(userToCorrect.equals(s.split("#")[0]))
+			{
+				String previousMessage = getLatestMessage(userToCorrect, storage.get(event.getChannel().getName()));
+				String newMessage = getLatestMessage(event.getUser().getNick(), storage.get(event.getChannel().getName())).replace(toReplace, replaceWith); //w/o italics
+				String correctedMessage = getLatestMessage(userToCorrect, storage.get(event.getChannel().getName())).replace(toReplace, "\u001d" + replaceWith + Colors.NORMAL); //w/ italics
+
+				if(previousMessage.equals(correctedMessage))
+					return;
+
+				if(correctsDifferentUser)
+					Utilities.chanMsg(event, Colors.NORMAL + userToCorrect + " " + L10N.getString("correction.1") + " " + event.getUser().getNick() + " " + L10N.getString("correction.2") + ": " + correctedMessage);
+				else
+					Utilities.chanMsg(event, Colors.NORMAL + userToCorrect + " " + L10N.getString("correction.3") + ": " + correctedMessage);
+
+				updateLatestMessage(event.getChannel().getName(), newMessage, userToCorrect);
+			}
+		}
+	}
 
 	/**
-	 * Inserts italics into the corrected parts of the message.
-	 * 
-	 * @param previousMessage - The message before it was corrected
-	 * @param newMessage - The message after it was corrected
-	 * @return - The message including italics
+	 * Adds or updates a user with his latest message
+	 * @param msg - The message to write into the array
+	 * @param username - The name of the user who wrote the message
 	 */
-	private static String addItalics(String previousMessage, String newMessage)
+	public static void updateLatestMessage(String channel, String msg, String username)
 	{
-		char[] p = previousMessage.toCharArray();
-		char[] n = newMessage.toCharArray();
-		String italicsMessage = "";
-		boolean italics = false;
-		int length;
-		boolean nLength;
+		int i = 0;
 
-		if(p.length > n.length)
-		{
-			length = p.length;
-			nLength = false;
-		}
-		else
-		{
-			length = n.length;
-			nLength = true;
-		}
+		if(!storage.containsKey(channel))
+			storage.put(channel, new ArrayList<String>());
 
-		for(int i = 0; i < length; i++) //iterate through the most
-		{
-			try
-			{
-				if(!nLength)
-				{
-					if(!italics)
-					{
-						if(p[i] == n[i])
-							italicsMessage += n[i];
-						else
-						{
-							italicsMessage += "\u001d";
-							italicsMessage += n[i];
-							italics = true;
-						}
-					}
-					else
-					{
-						if(p[i] != n[i])
-							italicsMessage += n[i];
-						else
-						{
-							italicsMessage += "\u001d";
-							italicsMessage += n[i];
-							italics = false;
-						}
-					}
-				}
-				else
-				{
-					if(i < p.length)
-					{
-						if(!italics)
-						{
-							if(p[i] == n[i])
-								italicsMessage += n[i];
-							else
-							{
-								italicsMessage += "\u001d";
-								italicsMessage += n[i];
-								italics = true;
-							}
-						}
-						else
-						{
-							if(p[i] != n[i])
-								italicsMessage += n[i];
-							else
-							{
-								italicsMessage += "\u001d";
-								italicsMessage += n[i];
-								italics = false;
-							}
-						}
-					}
-					else
-					{
-						if(!italics)
-						{
-							italicsMessage += "\u001d";
-							italicsMessage += n[i];
-							italics = true;
-						}
-						else
-							italicsMessage += n[i];
-					}
-				}
-			}
-			catch(ArrayIndexOutOfBoundsException e)
-			{
+		for(String s : storage.get(channel))
+		{ 
+			//if the current array position contains no data to replace, stop the loop and add the data
+			if(s == null)
 				break;
+
+			//checking for the correct array position to potentially replace the latest message with
+			if(s.split("#")[0].equals(username))
+			{
+				storage.get(channel).set(i, username + "#" + msg);
+				return;
 			}
+
+			i++;
 		}
-		
-		italics = false; //just in case
-		
-		return italicsMessage;
+
+		storage.get(channel).add(username + "#" + msg);
+	}
+
+	/**
+	 * Returns the latest message from the given user
+	 * @param user - The name of the user to get the latest message from
+	 * @return - The latest message from the given user
+	 */
+	private static String getLatestMessage(String user, List<String> messages)
+	{
+		for(String s : messages)
+		{
+			if(s.split("#")[0].equals(user))
+				return s.split("#")[1];
+		}
+
+		return "Will never happen.";
 	}
 }
