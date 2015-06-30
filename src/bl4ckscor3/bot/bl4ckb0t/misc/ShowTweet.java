ff5c91784aeb569bcf43f4aa80628fd23efe5a8a
bl4ckscor3 <donalddani9612@gmx.de>
2015-06-30 23:50:59 +0200

    fixed "Verified Account" showing

--- src/bl4ckscor3/bot/bl4ckb0t/misc/ShowTweet.java
+++ src/bl4ckscor3/bot/bl4ckb0t/misc/ShowTweet.java
@@ -2,7 +2,7 @@ package bl4ckscor3.bot.bl4ckb0t.misc;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.pircbotx.Colors;
 import org.pircbotx.hooks.events.MessageEvent;
 
@@ -13,19 +13,26 @@ public class ShowTweet
 {
 	public static void show(MessageEvent<Bot> event, String link)
 	{
-		WebDriver driver = new HtmlUnitDriver(false);
+		WebDriver driver = new FirefoxDriver();
 		String name = "";
 		String account = "";
 		String tweet = "";
-		
+
 		if(link.startsWith("www."))
 			link = "http://" + link;
 
 		if(link.split("/").length < 6) //it's not a tweet
 			return;
-		
+
 		driver.get(link);
 		name = driver.findElement(By.xpath("//strong[@class='fullname js-action-profile-name show-popup-with-id']")).getText();
+
+		if(name.endsWith("Verified Account"))
+		{
+			name = name.substring(0, name.lastIndexOf("Verified Account"));
+			name = name.substring(0, name.length() - 1);
+		}
+
 		account = driver.findElement(By.xpath("//span[@class='username js-action-profile-name']")).getText();
 		tweet = driver.findElement(By.xpath("//p[@class='TweetTextSize TweetTextSize--28px js-tweet-text tweet-text']")).getText();
 		driver.close();
