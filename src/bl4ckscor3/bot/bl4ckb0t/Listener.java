b03e549a1a3d6f6d4a24d170a41471198115eb8e
bl4ckscor3 <donalddani9612@gmx.de>
2016-09-28 21:24:34 +0200

    added private commands

--- src/bl4ckscor3/bot/bl4ckb0t/Listener.java
+++ src/bl4ckscor3/bot/bl4ckb0t/Listener.java
@@ -2,8 +2,10 @@ package bl4ckscor3.bot.bl4ckb0t;
 
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
+import org.pircbotx.hooks.events.PrivateMessageEvent;
 
 import bl4ckscor3.bot.bl4ckb0t.commands.BaseChannelCommand;
+import bl4ckscor3.bot.bl4ckb0t.commands.BasePrivateCommand;
 import bl4ckscor3.bot.bl4ckb0t.logging.Logging;
 import bl4ckscor3.bot.bl4ckb0t.util.Utilities;
 
@@ -26,6 +28,33 @@ public class Listener extends ListenerAdapter
 				for(BaseChannelCommand cmd : m.getChannelCommands())
 				{
 					if(cmd.isValidAlias(cmdName))
+					{
+						try
+						{
+							cmd.exe(event, cmdName, Utilities.toArgs(event.getMessage()));
+						}
+						catch(Exception e)
+						{
+							Logging.stackTrace(e);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	@Override
+	public void onPrivateMessage(PrivateMessageEvent event) throws Exception
+	{
+		String cmdName = event.getMessage().split(" ")[0];
+
+		for(Module m : Modules.modules)
+		{
+			if(m.hasPrivateCommand())
+			{
+				for(BasePrivateCommand cmd : m.getPrivateCommands())
+				{
+					if(cmdName.equals(cmd.getMainAlias()))
 					{
 						try
 						{
