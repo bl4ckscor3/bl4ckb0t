2ffde4982f99e11030bd1655d311ff3399ada6bb
bl4ckscor3 <donalddani9612@gmx.de>
2016-10-07 01:56:31 +0200

    added youtube module

---
+++ YouTube/src/bl4ckscor3/module/youtube/YouTube.java
@@ -0,0 +1,81 @@
+package bl4ckscor3.module.youtube;
+
+import java.net.URLClassLoader;
+
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.pircbotx.hooks.events.MessageEvent;
+
+import bl4ckscor3.bot.bl4ckb0t.Module;
+import bl4ckscor3.bot.bl4ckb0t.commands.BaseChannelCommand;
+import bl4ckscor3.bot.bl4ckb0t.l10n.L10N;
+import bl4ckscor3.bot.bl4ckb0t.util.Utilities;
+
+public class YouTube extends Module
+{
+	private L10N l10n;
+	
+	public YouTube(String name)
+	{
+		super(name);
+	}
+
+	@Override
+	public void onEnable(URLClassLoader loader)
+	{
+		getBuilder().registerChannelCommand(this, new Command(this));
+		l10n = new L10N(this, loader);
+	}
+	
+	@Override
+	public String[] getUsage(String channel)
+	{
+		return new String[]{
+				l10n.translate("explanation", channel)
+		};
+	}
+	
+	public class Command extends BaseChannelCommand
+	{
+		private Module module;
+		
+		public Command(Module m)
+		{
+			module = m;
+		}
+		
+		@Override
+		public void exe(MessageEvent event, String cmdName, String[] args) throws Exception
+		{
+			String channel = event.getChannel().getName();
+			
+			if(args.length == 1)
+			{				
+				try
+				{
+					Jsoup.connect("http://www.youtube.com/" + args[0]).get();
+					Utilities.sendMessage(channel, "http://www.youtube.com/" + args[0]);
+				}
+				catch(HttpStatusException e)
+				{
+					if(e.getStatusCode() == 404)
+						Utilities.sendMessage(channel, l10n.translate("error", channel));
+				}
+			}
+			else
+				Utilities.sendHelp(module, event.getUser().getNick(), channel);
+		}
+
+		@Override
+		public String[] getAliases()
+		{
+			return new String[]{"youtube", "yt"};
+		}
+
+		@Override
+		public String getSyntax(String channel)
+		{
+			return l10n.translate("syntax", channel);
+		}
+	}
+}
