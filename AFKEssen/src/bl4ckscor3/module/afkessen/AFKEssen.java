595561500f691dff99fcdffb1ffef1131c9fc15d
bl4ckscor3 <donalddani9612@gmx.de>
2016-10-15 19:16:46 +0200

    added afkessen module (so silly and unneeded yet so great)

---
+++ AFKEssen/src/bl4ckscor3/module/afkessen/AFKEssen.java
@@ -0,0 +1,45 @@
+package bl4ckscor3.module.afkessen;
+
+import java.net.URLClassLoader;
+
+import org.pircbotx.hooks.ListenerAdapter;
+import org.pircbotx.hooks.events.MessageEvent;
+
+import bl4ckscor3.bot.bl4ckb0t.Module;
+import bl4ckscor3.bot.bl4ckb0t.l10n.L10N;
+import bl4ckscor3.bot.bl4ckb0t.util.Utilities;
+
+public class AFKEssen extends Module
+{
+	private L10N l10n;
+	
+	public AFKEssen(String name)
+	{
+		super(name);
+	}
+
+	@Override
+	public void onEnable(URLClassLoader loader)
+	{
+		getBuilder().addListener(new Listener());
+		l10n = new L10N(this, loader);
+	}
+	
+	@Override
+	public String[] getUsage(String channel)
+	{
+		return new String[]{
+				l10n.translate("explanation", channel)
+		};
+	}
+	
+	public class Listener extends ListenerAdapter
+	{
+		@Override
+		public void onMessage(MessageEvent event) throws Exception
+		{
+			if(event.getMessage().matches("afk[A-Za-z]*essen"))
+				Utilities.sendMessage(event.getChannel().getName(), "gh");
+		}
+	}
+}
